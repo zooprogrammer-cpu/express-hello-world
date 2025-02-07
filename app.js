47e321989e632093991cef876a901425adf5680a
ABGitRepo <60715599+zooprogrammer-cpu@users.noreply.github.com>
2025-02-07 10:07:37 -0600

    Corrected arrow function

--- app.js
+++ app.js
@@ -5,9 +5,10 @@ const port = process.env.PORT || 3001;
 app.get("/", (req, res) => res.type('html').send(html));
 
 // Add a new route for the health endpoint
-app.get("/health", (req, res)) =>{
+app.get("/health", (req,res)=> {
   res.sendStatus(200);
 });
+
 
 const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
